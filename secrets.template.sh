5f1f34a77bc66861da675d1eaeee68de4914daaf
George Rey <grey@stl-tech.co.uk>
2022-04-18 20:24:38 +0100

    auto

--- secrets.template.sh
+++ secrets.template.sh
@@ -1,11 +1,11 @@
-export RASPI_IP=|secret|
-export REYMONTE_IP=|secret|
-export PAPERSPACE_APIKEY=|secret|
-export PAPERSPACE_REYMONTE_MACHINENAME=|secret|
-export UZDROWICIEL_SFTP_ACCOUNTNAME=|secret|
+export RASPI_IP=secret
+export REYMONTE_IP=secret
+export PAPERSPACE_APIKEY=secret
+export PAPERSPACE_REYMONTE_MACHINENAME=secret
+export UZDROWICIEL_SFTP_ACCOUNTNAME=secret
 export BOXNAME="=set me in secrets.sh="
 
 
-export REYMONTE_IMAGE_REGISTRY_ADDRESS=|secret|
-export REYMONTE_IMAGE_REGISTRY_USER=|secret|
-export REYMONTE_IMAGE_REGISTRY_PASS=|secret|
+export REYMONTE_IMAGE_REGISTRY_ADDRESS=secret
+export REYMONTE_IMAGE_REGISTRY_USER=secret
+export REYMONTE_IMAGE_REGISTRY_PASS=secret
