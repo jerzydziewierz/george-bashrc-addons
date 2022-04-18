48e249bb81cc207c37abe237c428d69f3fc3b338
George Rey <grey@stl-tech.co.uk>
2022-04-18 09:28:12 +0100

    auto

--- george-bashrc-addons.sh
+++ george-bashrc-addons.sh
@@ -12,7 +12,7 @@
 # load reymonte commands
 . george-bashrc-addons-reymonte.sh
 . george-bashrc-addons-devel-tools.sh
-. fancy-prompt
+. fancy-prompt.sh
 # private most often used links
 . george-bashrc-addons-links.sh
 
