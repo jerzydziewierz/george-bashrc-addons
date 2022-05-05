36227f54ed3762e2e8359f747e50b03c440f087d
George Rey <1606347+jerzydziewierz@users.noreply.github.com>
2022-05-05 15:55:55 +0100

    update benchmarks

--- secrets.template.sh
+++ secrets.template.sh
@@ -12,7 +12,8 @@ export REYMONTE_IMAGE_REGISTRY_PASS=secret
 
 ## little list of benchmarks of cloud CPUs
 # 7zip b
-# AMD Ryzen 9 3900X 12-Core Processor (24 cores booked) :                   2229   3614  80643
-# AMD EPYC 7642 48-Core Processor, (22 cores booked):                       1807   3646  65944
-# Intel(R) Xeon(R) CPU E5-2620 v4 @ 2.10GHz (406F1)  (4 cores booked) :      377   2687  10084
-# Intel(R) Core(TM) i7-8665U CPU @ 1.90GHz (806EC) (8 cores) 		             707   1981  13822
+# AMD Ryzen 9 3900X 12-Core Processor (24 cores booked) :                             2229   3614  80643
+# AMD EPYC 7642 48-Core Processor, (22 cores booked):                                 1807   3646  65944
+# Intel(R) Xeon(R) CPU E5-2620 v4 @ 2.10GHz (406F1)  (4 cores booked) :                377   2687  10084
+# Intel(R) Core(TM) i7-8665U CPU @ 1.90GHz (806EC) (8 cores) 		                       707   1981  13822
+# Paperspace Intel(R) Xeon(R) CPU E5-2620 v4 @ 2.10GHz (24 out of 32 cores booked):   2152   2023  43421
