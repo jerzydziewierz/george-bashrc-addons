16aa12683df9d54be09868fe43a2dcbae30da7c8
George Rey <grey@stl-tech.co.uk>
2022-05-08 09:26:29 +0000

    autosave

--- secrets.template.sh
+++ secrets.template.sh
@@ -3,7 +3,7 @@ export REYMONTE_IP=secret
 export PAPERSPACE_APIKEY=secret
 export PAPERSPACE_REYMONTE_MACHINENAME=secret
 export UZDROWICIEL_SFTP_ACCOUNTNAME=secret
-export BOXNAME="\e[53m\e[4m|set me|\e[55m"
+export BOXNAME="\e[53m\e[4m|set me${BG_DARKGREY1}|\e[55m"
 
 
 export REYMONTE_IMAGE_REGISTRY_ADDRESS=secret
@@ -12,8 +12,11 @@ export REYMONTE_IMAGE_REGISTRY_PASS=secret
 
 ## little list of benchmarks of cloud CPUs
 # 7zip b
-# AMD Ryzen 9 3900X 12-Core Processor (24 cores booked) :                             2229   3614  80643
-# AMD EPYC 7642 48-Core Processor, (22 cores booked):                                 1807   3646  65944
-# Intel(R) Xeon(R) CPU E5-2620 v4 @ 2.10GHz (406F1)  (4 cores booked) :                377   2687  10084
-# Intel(R) Core(TM) i7-8665U CPU @ 1.90GHz (806EC) (8 cores) 		                       707   1981  13822
-# Paperspace Intel(R) Xeon(R) CPU E5-2620 v4 @ 2.10GHz (24 out of 32 cores booked):   2152   2023  43421
+# provider          | CPU                                       | cores booked| CPU used | core perf | all cores perf
+# R3 desktop        | AMD Ryzen 9 3900X 12-Core Processor               | 24C |     2229 |      3614 | 80643
+# TP8 laptop        | Intel(R) Core(TM) i7-8665U CPU @ 1.90GHz (806EC)  | 8C  |	     707 |      1981 | 13822
+# Paperspace C??    | Intel(R) Xeon(R) CPU E5-2620 v4 @ 2.10GHz (406F1) | 4C  |      377 |      2687 | 10084
+# ???????           | AMD EPYC 7642 48-Core Processor,                  | 22C |     1807 |      3646 | 65944
+# Paperspace C9     | Intel(R) Xeon(R) CPU E5-2620 v4 @ 2.10GHz (406F1) | 24C |     2128 |      2025 | 43004
+# Paperspace C9     | Intel(R) Xeon(R) CPU E5-2620 v4 @ 2.10GHz         | 24C |     2152 |      2023 | 43421
+# Datacrunch CPU32V | AMD EPYC-Rome Processor (830F10)                  | 32C |     2839 |      3443 | 97603
