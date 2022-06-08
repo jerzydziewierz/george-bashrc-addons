262cf2f23c9d14a4264bb5e69a80c0c2c3a3fded
George Rey <1606347+jerzydziewierz@users.noreply.github.com>
2022-06-08 17:24:32 +0100

    add zellij

---
+++ ubuntu-prep/default-cli/zellij.sh
@@ -0,0 +1,4 @@
+# zellij is a tabbed terminal with detach support. Combines tmux and screen, and does more.
+# from https://github.com/zellij-org/zellij
+# assuming that rust is installed:
+cargo install zellij
